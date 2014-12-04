8b2fdc4d20e86d85d207cda7cf14731a2c2d16ee
Satish522 <satishsubudhi522@gmail.com>
2014-12-04 05:41:23 -0800

    Spring Core

---
+++ SpringDemo/src/spring/demo/DrawingApp.java
@@ -0,0 +1,24 @@
+package spring.demo;
+
+ 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//It's a bean factory concept to get bean
+	//	BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle=(Triangle)context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
---
+++ SpringDemo/src/spring/demo/Triangle.java
@@ -0,0 +1,76 @@
+package spring.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware,BeanNameAware {
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	private ApplicationContext context;
+	
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+
+	public void draw(){
+		  System.out.println("Point Value is ("+getPointA().getX()+","+getPointA().getY()+")");
+		  System.out.println("Point Value is ("+getPointB().getX()+","+getPointB().getY()+")");
+		  System.out.println("Point Value is ("+getPointC().getX()+","+getPointC().getY()+")");
+	 }
+
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		// TODO Auto-generated method stub
+		this.context=context;
+		System.out.println(context);
+		System.out.println(context.toString());
+	}
+
+
+
+	@Override
+	public void setBeanName(String beanName) {
+		// TODO Auto-generated method stub
+		System.out.println("Bean Name is "+beanName);
+	}
+}
