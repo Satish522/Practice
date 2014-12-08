a2727d732726bfb2311e4f9a38cd1c2b7b19e553
$@T!$#_2 <$@T!$#_2@SATISH-PC>
2014-12-08 17:44:23 +0530

    Annotation with qualifier

--- SpringDemo/src/spring/demo/Circle.java
+++ SpringDemo/src/spring/demo/Circle.java
@@ -1,25 +1,29 @@
 package spring.demo;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
 	
-	private Point pointA;
-	
-	public Point getPointA() {
-		return pointA;
+	private Point center;
+	 
+	public Point getCenter() {
+		return center;
 	}
-	
 	@Autowired
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
+	@Qualifier("circleRelated")
+	public void setCenter(Point center) {
+		this.center = center;
 	}
+
+
+
 
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
 		System.out.println("Drawing Circle.....");
-		System.out.println("Co-ordinates are {"+getPointA().getX()+" , "+getPointA().getY()+"}");
+		System.out.println("Co-ordinates are {"+getCenter().getX()+" , "+getCenter().getY()+"}");
 	}
 
 }
