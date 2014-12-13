cf26fc2b714b326af47637a2cf406230aa829d42
$@T!$#_2 <$@T!$#_2@SATISH-PC>
2014-12-13 11:04:01 +0530

    Error Commit

--- SpringApp/src/main/java/org/isol/satish/MainApp.java
+++ SpringApp/src/main/java/org/isol/satish/MainApp.java
@@ -1,5 +1,6 @@
 package org.isol.satish;
 
+
 import org.isol.satish.service.ShapeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,8 +10,10 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		
 		ShapeService shapeService= context.getBean("shapeService",ShapeService.class);
 		System.out.println(shapeService.getTriangle().getName());
+		
 	}
 
 }
